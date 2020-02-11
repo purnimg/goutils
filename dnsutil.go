650659b7ffec1ec5f6c9647d379dd131179de243
Purnima Gunturu <g.purnima@gmail.com>
2020-02-10 23:59:03 -0800

    DNS function

---
+++ dnsutil.go
@@ -0,0 +1,27 @@
+/* DNS lib funstion in go */
+
+package main
+
+import(
+				"fmt"
+				"net"
+				"os"
+)
+func main() {
+				if len(os.Args) != 2 {
+								fmt.Fprintf(os.Stderr, "Usage: %s hostname\n", os.Args[0])
+								os.Exit(1)
+				}
+
+				name := os.Args[1]
+
+				addr, err := net.ResolveIPAddr("ip", name)
+
+				if err!= nil {
+								fmt.Println("Resolution error", err.Error())
+								os.Exit(1)
+				}
+
+				fmt.Println("Resolved Address is ", addr.String())
+				os.Exit(0)
+}
